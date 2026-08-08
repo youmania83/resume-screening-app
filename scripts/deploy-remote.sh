ea0ed727e63ab6582392e8e3d746917ec60c534e
yk <y.wadhwa@icloud.com>
2026-08-08 22:01:29 +0530

    fix(deploy): clean .next and node_modules before npm ci in deploy-remote.sh

--- scripts/deploy-remote.sh
+++ scripts/deploy-remote.sh
@@ -180,6 +180,7 @@ git reset --hard 'origin/${GIT_BRANCH}'
 
 echo '==> 2/6 Installing dependencies...'
 pm2 stop all || true
+rm -rf .next node_modules
 NODE_ENV=development npm ci --include=dev || NODE_ENV=development npm install --include=dev
 
 echo '==> 3/6 Running type checks & regression suite...'
